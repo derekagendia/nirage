28e954d5661c899860fa7ced68145d47394ce01b
Stevy Marlino <stevyabessolo@gmail.com>
2021-06-14 10:31:29 +0100

    Update ContactController.php
    add contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\VisitRequestEvent;
+use App\Models\Apartement;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -11,6 +13,15 @@ class ContactController extends Controller
         $data = [
             'title' => 'Contact'
         ];
-        return view('nirage.contact',$data);
+        return view('nirage.contact', $data);
+    }
+
+    public function send(Request $request)
+    {
+        $request = $request->all();
+        $apartment = Apartement::where('slug', $request['slug'])->firstOrfail();
+        // Event
+
+        event(new VisitRequestEvent($request, $apartment));
     }
 }
