e958a7b592f99642e530080157d5aca5f052ffb5
StevyMarlino <stevyabessolo@gmail.com>
2021-06-16 13:12:28 +0200

    Update contact.blade.php

--- resources/views/nirage/contact.blade.php
+++ resources/views/nirage/contact.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.app')
+<section class="page-header-1" style="background-image:url({{ asset('assets/img/apartments/cover.jpg') }});">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h2 class="display-4 fw-bolder text-white text-center" style="padding-top:10rem;">Contact</h2>
+        </div>
+    </div>
+</div>
+</section>
